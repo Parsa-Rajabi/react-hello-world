a797cd1a153a3d2f3eb396a2c597fa4a64f9d568
Parsa <ParsaRajabiPR@gmail.com>
2020-10-14 13:35:23 -0700

    removed app, added hello world

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <h1>Hello world from 2020!</h1>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
